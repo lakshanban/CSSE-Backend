9160583d81a8a2434bdc2b64948a63ec11566645
unknown <13asanka13@gmail.com>
2020-10-05 22:11:10 +0530

    request api's added

--- src/middleware/routes.js
+++ src/middleware/routes.js
@@ -1,12 +1,31 @@
 const login = require('../controllers/loginController').login
+const { reqConGetAllRequests, reqConGetRequestById, reqConGetRequestByCreator } = require('../controllers/requestController')
 
 
 const routes = [
     {
         path: '/login',
-        handler: login,
+        handler: login,                        // receives username and password
+        method: 'POST'
+    },
+
+    {
+        path: '/getallrequests',
+        handler: reqConGetAllRequests,
+        method: 'GET'
+    },
+
+    {
+        path: '/getreqbyid',
+        handler: reqConGetRequestById,         // receives RID (request ID)
+        method: 'POST'
+    },
+
+    {
+        path: '/getreqbycreator',
+        handler: reqConGetRequestByCreator,    // receives cid (creator id)
         method: 'POST'
     }
 ]
 
-module.exports = {routes}
+module.exports = {routes} 
